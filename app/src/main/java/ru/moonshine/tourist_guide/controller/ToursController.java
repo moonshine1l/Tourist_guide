6277c5a8fdf0dd6ff1e4e631945d2f55f2344691
moonshine1l <norka5imen@gmail.com>
2023-05-31 23:20:17 +0300

    v1

---
+++ app/src/main/java/ru/moonshine/tourist_guide/controller/ToursController.java
@@ -0,0 +1,64 @@
+package ru.moonshine.tourist_guide.controller;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ru.moonshine.tourist_guide.R;
+import ru.moonshine.tourist_guide.model.Tours;
+
+public class ToursController extends RecyclerView.Adapter<ToursController.ToursView> {
+
+    Context context;
+    List<Tours> tours;
+
+    public ToursController(Context context, List<Tours> tours) {
+        this.context = context;
+        this.tours = tours;
+    }
+
+    @NonNull
+    @Override
+    public ToursView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View tourItems = LayoutInflater.from(context).inflate(R.layout.recycler_view_item, parent, false);
+        return new ToursController.ToursView(tourItems);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ToursView holder, int position) {
+        int imageId = context.getResources().getIdentifier(tours.get(position).getImage(), "drawable", context.getPackageName());
+
+        holder.tourImage.setImageResource(imageId);
+        holder.tourTitle.setText(tours.get(position).getTitle());
+        holder.tourPrice.setText(tours.get(position).getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tours.size();
+    }
+
+    public static final class ToursView extends RecyclerView.ViewHolder{
+
+        ImageView tourImage;
+        TextView tourTitle;
+        TextView tourPrice;
+
+        public ToursView(@NonNull View itemView) {
+            super(itemView);
+
+            tourImage = itemView.findViewById(R.id.ImageView);
+            tourTitle = itemView.findViewById(R.id.TourTitle);
+            tourPrice = itemView.findViewById(R.id.tourPrice);
+        }
+    }
+}
---
+++ app/src/main/java/ru/moonshine/tourist_guide/model/Tours.java
@@ -0,0 +1,47 @@
+package ru.moonshine.tourist_guide.model;
+
+public class Tours {
+    int id;
+    String title;
+    String image;
+    String price;
+
+    public Tours(int id, String title, String image, String price) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
